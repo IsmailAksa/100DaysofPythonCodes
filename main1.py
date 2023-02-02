ace917efbbeda78309b1476fb81adc038bd0bd9b
Aksa Ismail <36132081+IsmailAksa@users.noreply.github.com>
2023-02-02 22:36:52 +0600

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,10 @@
+# 100DaysofCodePython
+"""
+Hey don't remove this line
+Author Aksa
+"""
+print("Hey I am a \"good lady\"\nand this code viewer also good lady/man")
+print("Hello Snake") # Printing Snake
+print("This is aprinting statement")
+print(" Hey", 6, 7, sep="~", end="008\n")
+print("Aksa")

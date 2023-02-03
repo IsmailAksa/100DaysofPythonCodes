d9f430050332e1c8a4e17005b0a992ffe3953392
Aksa Ismail <36132081+IsmailAksa@users.noreply.github.com>
2023-02-04 01:18:28 +0600

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,15 @@
+a= complex(8, 2)
+b= True
+c= "Aksa"
+d= None
+print(a)
+print(b)
+a1=9
+print(a+a1)
+print("The type of a is", type(a))
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+list1 = [ 8, 2.3,[-4, 5],["apple","banana"]]
+print(list1)
+tuple1= (("parrot"), ("lion"))
+print(tuple1)

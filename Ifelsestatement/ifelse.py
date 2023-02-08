27c9958b80a1d4afc0f8246866969df090ec2cee
Aksa Ismail <36132081+IsmailAksa@users.noreply.github.com>
2023-02-09 00:43:47 +0600

    ifelse is added

---
+++ Ifelsestatement/ifelse.py
@@ -0,0 +1,27 @@
+#a = int(input("Enter your age "))
+#print("Your age is:", a)
+
+# conditional operators
+# >, <, >=, =>, ==, !=
+# print(a>18)
+#print(a<=18)
+#print(a==18)
+#print(a!=18)
+
+#if (a > 18):
+ # print("You can drive")
+  #print("Yes")
+#else:
+ # print("You can not drive")
+  #print("NO")
+  
+#elif
+
+num = int(input("Enter the value of num:"))
+if (num < 0):
+  print("Number is negative.")
+elif (num==0):
+  print("Number is Zero.")
+else:
+ print("Number is positive.")
+ print("I am happy now")

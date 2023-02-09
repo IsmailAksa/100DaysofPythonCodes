f7061bbede831bb9d58b51f8fa1b61bb382634ea
Aksa Ismail <36132081+IsmailAksa@users.noreply.github.com>
2023-02-10 01:32:50 +0600

    forloop and whileloop added

---
+++ loop/forloop.py
@@ -0,0 +1,18 @@
+#name = "Muaz"
+#for i in name:
+ #print(i)
+  #if (i== "a"):
+   # print("This is something special")
+#colors = ["Red","Green","Blue","Yellow"]
+#for color  in colors:
+#   print(color)
+#    for i in color:
+#     print(i)
+     
+#for k in range(20):
+  #print(k+1)
+#for k in range(21,29):
+ # print(k)
+# third parameter start stop and step
+for k in range(1,12,3):
+   print(k)
---
+++ loop/whileloop.py
@@ -0,0 +1,18 @@
+#i=0
+#while(i<3):
+#print(i)
+#  i= i+1
+#print("Done with the loop")
+#i = int(input("Enter the number: "))
+#while(i<=38):
+# i = int(input("Enter the number: "))
+# print(i)
+ 
+#print("Done with the loop")
+
+count = 5
+while(count >0):
+  print(count)
+  count = count-1
+else:
+  print("else is 0")

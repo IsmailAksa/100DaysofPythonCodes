f32332e8cae028928eb7bd8df49b08303aa68efb
Aksa Ismail <36132081+IsmailAksa@users.noreply.github.com>
2023-02-10 20:55:28 +0600

    Add files via upload

---
+++ loop/breakandcontinue.py
@@ -0,0 +1,22 @@
+#for i in range(12):
+# if( i==10 ):
+#  break
+# print("5 X", i+1, "=", 5 * (i+1))
+   
+
+#print("Leave the Loop")
+
+#for i in range(12):
+#  if( i==10 ):
+#    print("skip the iteration")
+#    continue
+#  print("5 X", i+1, "=", 5 * (i+1))
+
+# do while loop emulate
+i = 0
+while True:
+  print(i)
+  i = i+1
+  if( i%100 ==0):
+    break
+  

35469b26d5fea608a1f52e036ccf516d66245bc6
Aksa Ismail <36132081+IsmailAksa@users.noreply.github.com>
2023-05-14 22:49:12 +0600

    warlus

---
+++ Warlus/Warlus_Operator.py
@@ -0,0 +1,14 @@
+# # print(a:=False)
+# numbers = [1,2,3,4,5]
+# while(n:=len(numbers))  > 0:
+#   print(numbers.pop())
+
+# foods = list()
+# while True:
+#   food = input("What do you like?:")
+#   if food== "quit":
+#     break
+#     foods.append(food)
+foods = list()
+while(food:=input("What do you like?: ")) != "quit":
+  foods.append(food)
